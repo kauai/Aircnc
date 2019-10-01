2a096ef9ee1dfb4f13a23a50007026429e057ffb
Thiago kauai <html5ephp@gmail.com>
2019-10-01 15:14:22 -0300

    criando Schema e rota Spot,e implementando validacao de email duplicado rota sessions

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -2,9 +2,16 @@
 const User = require('../models/User')
 
 module.exports = {
+    //FILTRANDO EMAIL SE JA FOR EXISTENTE
     async store(req,res) {
-        const { email } = req.body
-        const user = await User.create({ email })
-        return res.json(user)
+       const { email } = req.body
+
+       let user = await User.findOne({ email })
+
+       if(!user) {
+           user = await User.create({ email })
+       }
+       
+       return res.json(user)
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const routes = express.Router()
 const SessionController = require('./controllers/SessionController')
+const SpotController = require('./controllers/SpotController')
 
 routes.post('/sessions',SessionController.store)
+routes.post('/spots',SpotController.store)
 
 module.exports = routes 

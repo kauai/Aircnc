75a8c7b46ca5469a84fc495d8299ec314ede6fb8
Thiago kauai <html5ephp@gmail.com>
2019-10-01 20:13:16 -0300

    Criando BookingController,DashboardController e model Booking e adicionando novas rotas np route.js

--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,15 @@ const uploadConfig = require('./config/upload')
 
 const SessionController = require('./controllers/SessionController')
 const SpotController = require('./controllers/SpotController')
+const DashBoardController = require('./controllers/DashboardController')
+const BookingController = require('./controllers/BookingController')
 
 const upload = multer(uploadConfig)
 
 routes.post('/sessions',SessionController.store)
 routes.get('/spots',SpotController.index)
 routes.post('/spots',upload.single('thumbnail'),SpotController.store)
+routes.get('/dashboard',DashBoardController.show)
+routes.post('/spots/:spot_id/bookings',BookingController.store)
 
 module.exports = routes 
